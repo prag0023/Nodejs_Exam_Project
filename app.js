cf075bc5631c42f5da8ea6bfbe65f627db1ceb6f
Nirendra Singh Rawal <nirenrawal@gmail.com>
2021-05-19 14:42:13 +0200

    created html classes added some design and added code to app.js file to serve web pages

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+const header = fs.readFileSync(__dirname + "/public/header/header.html", "utf-8");
+const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
+
+const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
+const register = fs.readFileSync(__dirname + "/public/register/register.html");
 
 
 app.get("/", (req,res) => {
-    res.send("hello peoplw");
+    res.send(header + frontpage + footer);
+});
+
+app.get("/register", (req, res) => {
+    res.send(header + register + footer);
 });
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
