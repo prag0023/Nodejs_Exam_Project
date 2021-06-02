a036b325473b031df0ccb0158b1e5b515962f58d
Nirendra Singh Rawal <nirenrawal@gmail.com>
2021-06-02 20:52:39 +0200

    changed the url of the fetch method from localhost to heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
     const express = require("express");
     const mongoose = require("mongoose");
     const fs = require("fs");
+    const dotenv = require('dotenv');
 
 
     const app = express();
